9212a0598e9f141669e8c090f80bcadcf0b866b7
Maksim Klychkov <maxim.klychkov64@gmail.com>
2024-02-13 16:44:21 +0300

    Added basic models. Connected reference to CLI project

---
+++ WidgetsJSON/Specification.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WidgetsJSON
+{
+    public class Specification
+    {
+        private string? _name;
+        private double? _price;
+        private bool? _isCustom;
+
+        [JsonPropertyName("specName")]
+        public string? Name { get { return _name; } private set { _name = value; } }
+
+        [JsonPropertyName("specPrice")]
+        public double? Price { get { return _price; } private set { _price = value; } }
+
+        [JsonPropertyName("isCustom")]
+        public bool? IsCustom { get { return _isCustom; } private set { _isCustom = value; } }
+
+        private Specification() { }
+
+        public Specification(string? name, double? price, bool? isCustom)
+        {
+            Name = name;
+            Price = price;
+            IsCustom = isCustom;
+        }
+    }
+}
---
+++ WidgetsJSON/Widget.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WidgetsJSON
+{
+    public class Widget
+    {
+        private string? _id;
+        private string? _name;
+        private int? _quantity;
+        private double? _price;
+        private bool? _isAvailable;
+        private DateTime? _manufactureDate;
+        private List<Specification>? _specifications;
+
+        [JsonPropertyName("widgetId")]
+        public string? Id { get { return _id; } private set { _id = value; } }
+
+        [JsonPropertyName("name")]
+        public string? Name { get { return _name; } private set { _name = value; } }
+
+        [JsonPropertyName("quantity")]
+        public int? Quantity { get { return _quantity; } private set { _quantity = value; } }
+
+        [JsonPropertyName("price")]
+        public double? Price { get { return _price; } private set { _price = value; } }
+
+        [JsonPropertyName("isAvailable")]
+        public bool? IsAvailable { get { return _isAvailable; } private set { _isAvailable = value; } }
+
+        [JsonPropertyName("manufactureDate")]
+        public DateTime? ManufactureDate { get { return _manufactureDate; } private set { _manufactureDate = value; } }
+
+        // Использовать копию для стороннего доступа, внутри -- поле.
+        [JsonPropertyName("specifications")]
+        public List<Specification>? Specifications { get { return _specifications; } private set { _specifications = value; } }
+
+        private Widget() { }
+
+        public Widget(string? id, string? name, int? quantity, double? price, bool? isAvailable, DateTime? manufactureDate, List<Specification>? specifications)
+        {
+            Id = id;
+            Name = name;
+            Quantity = quantity;
+            Price = price;
+            IsAvailable = isAvailable;
+            ManufactureDate = manufactureDate;
+            Specifications = specifications;
+        }
+    }
+}
