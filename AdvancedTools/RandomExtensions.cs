06b9f1d5b9cee1cb7ac1761e8e34cd4e64c6395b
Maksim Klychkov <maxim.klychkov64@gmail.com>
2024-02-20 01:09:46 +0300

    Advanced tools created and Random Extension added

---
+++ AdvancedTools/RandomExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace AdvancedTools
+{
+    public static class RandomExtensions
+    {
+        public static void Shuffle<T>(this Random random, List<T> lst)
+        {
+            int n = lst.Count;
+            while (n > 1)
+            {
+                int k = random.Next(n--);
+                (lst[k], lst[n]) = (lst[n], lst[k]);
+            }
+        }
+    }
+}
