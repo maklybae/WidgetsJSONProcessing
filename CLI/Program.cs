beb874ced3c4b09f11743c8e2b56ea5f97c6c346
Maksim Klychkov <maxim.klychkov64@gmail.com>
2024-02-20 10:41:58 +0300

    Test data

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace CLI
+﻿using System.Text.Json;
+using WidgetsJSON;
+
+namespace CLI
 {
     internal class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            string fileName = "D:\\VisualStudio\\WidgetsJSONProcessing\\test.json";
+            string jsonString = File.ReadAllText(fileName);
+            List<Widget>? widgets = JsonSerializer.Deserialize<List<Widget>>(jsonString);
+
+            foreach (var item in widgets)
+            {
+                Console.WriteLine(item.Name);
+            }
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            jsonString = JsonSerializer.Serialize(widgets, options);
+
+            Console.WriteLine(jsonString);
         }
     }
 }
