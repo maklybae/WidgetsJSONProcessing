723b2bc3ac3bb3d9fbe9c63bef765e68e924aafe
Maksim Klychkov <maxim.klychkov64@gmail.com>
2024-02-24 17:46:44 +0300

    Author info

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -6,6 +6,10 @@ internal class Program
         // In summary, the program provides a framework for managing widgets, specifications,
         // and associated events, including automatic saving and price redistribution functionalities.
         // The solution is provided by human-readable user-friendly pretty CLI.
+
+        // Klychkov Maksim, 2024.
+        // https://github.com/maklybae/WidgetsJSONProcessing
+
         Controller.ShowMenu();
     }
 }
